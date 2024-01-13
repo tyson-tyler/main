6dfc3b79d1f1b1e85bcae9e6996b9cc86d739bae
benword <benword25@gmail.com>
2024-01-13 20:23:28 +0530

    logo

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -75,7 +75,7 @@ const SignupForm = () => {
   return (
     <Form {...form}>
       <div className="sm:w-420 flex-center flex-col">
-        <img src="/assets/images/1.png" alt="logo"  />
+        <img src="/assets/images/1.png" alt="logo" width={250} height={250}  />
 
         <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">
           Create a new account
